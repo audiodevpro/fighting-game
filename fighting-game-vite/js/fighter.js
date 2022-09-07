b0668ee8a2347c0f3a4b3c4dafb83be741184b10
ts_code <112668528+audiodevpro@users.noreply.github.com>
2022-09-07 14:15:18 -0700

    Added Attack Animation and Functionality to Fighter Sprite

--- fighting-game-vite/js/fighter.js
+++ fighting-game-vite/js/fighter.js
@@ -62,6 +62,7 @@ class Fighter extends Sprite {
   }
 
   attack() {
+    this.switchSprite('attack1')
     this.isAttacking = true
     setTimeout(() => {
       this.isAttacking = false
@@ -69,6 +70,12 @@ class Fighter extends Sprite {
   }
 
   switchSprite(sprite) {
+    if (
+      this.image === this.sprites.attack1.image &&
+      this.framesCurrent < this.sprites.attack1.framesMax - 1
+    )
+      return
+
     switch (sprite) {
       case 'idle':
         if (this.image !== this.sprites.idle.image)
@@ -76,6 +83,7 @@ class Fighter extends Sprite {
         this.framesMax = this.sprites.idle.framesMax
         this.framesCurrent = 0
         break
+
       case 'run':
         if (this.image !== this.sprites.run.image) {
           this.image = this.sprites.run.image
@@ -83,6 +91,7 @@ class Fighter extends Sprite {
           this.framesCurrent = 0
         }
         break
+
       case 'jump':
         if (this.image !== this.sprites.jump.image) {
           this.image = this.sprites.jump.image
@@ -90,10 +99,19 @@ class Fighter extends Sprite {
           this.framesCurrent = 0
         }
         break
+
       case 'fall':
         if (this.image !== this.sprites.fall.image) {
           this.image = this.sprites.fall.image
           this.framesMax = this.sprites.fall.framesMax
+          this.framesCurrent = 0
+        }
+        break
+
+      case 'attack1':
+        if (this.image !== this.sprites.attack1.image) {
+          this.image = this.sprites.attack1.image
+          this.framesMax = this.sprites.attack1.framesMax
           this.framesCurrent = 0
         }
         break
